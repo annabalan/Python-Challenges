e6c73bc14717f0473a563946ec6348d5f1377ac3
Anna Balan <colnyshkasmile@gmail.com>
2018-03-25 21:10:43 -0700

    user input added

--- Practice-Python/ex-03.py
+++ Practice-Python/ex-03.py
@@ -1,9 +1,12 @@
 #  Take a list, say for example this one: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] and write a program that prints out all the elements of the list that are less than 5.
 numbers = [1, 3, 3, 5, 7, 11, 13, 17, 39]
+numbers_2 = []
+userinput = int(input("Enter a number: "))
 def below_five(numbers):
     for number in numbers:
-        if number <= 5:
-            print(number)
+        if number <= userinput:
+            numbers_2.append(number)
+            print(numbers_2)
         else:
-            print("Number greater than 5.")
+            print("Number greater than", str(userinput))
 below_five(numbers)
