bde6bf74907da8493800a7ab070417b0e094f611
Anna Balan <colnyshkasmile@gmail.com>
2018-03-29 21:34:49 -0700

    range list changed start to 1

--- Practice-Python/ex-04.py
+++ Practice-Python/ex-04.py
@@ -1,12 +1,11 @@
 #Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 
 num = int(input("Choose a number: "))
-x = range(0, num)
+x = list(range(1, num))
 divisor_list = []
-def divisors(num):
-    if num % x == 0:
-        divisor_list.append(num)
-        print(divisor_list)
-    else:
+for number in x:
+    if num % number == 0:
+        divisor_list.append(number)
+print(divisor_list)
 
 
