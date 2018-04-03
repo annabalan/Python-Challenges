dd729d15bb7da0704da66c765272afa7ce4a6137
Anna Balan <colnyshkasmile@gmail.com>
2018-04-02 22:16:08 -0700

    ex-06.py

---
+++ Practice-Python/ex-06.py
@@ -0,0 +1,7 @@
+# Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+user_input = input("Enter a string: ")
+while user_input != " ":
+    if user_input
+        print("This is a palindrome.")
+    else:
+        print("This is not a palindrome.")
