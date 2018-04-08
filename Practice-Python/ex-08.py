096341ccae27f03734eb293ed8934f24dcf722c9
Anna Balan <colnyshkasmile@gmail.com>
2018-04-07 20:15:10 -0700

    rock-paper-scissors

--- Practice-Python/ex-08.py
+++ Practice-Python/ex-08.py
@@ -1,7 +1,11 @@
 #Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
 #Remember the rules:
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+print("/n/n/t/t/t'Welcome to Rock-Paper-Scissors.")
+print("/nWhen promted below, enter your move below. You can choose Rock, Paper or Scissors. Remember Rock beats Scissors. Scissors beats paper and Paper beats Rock.")
 
- #   Rock beats scissors
-  #  Scissors beats paper
-   # Paper beats rock
+player_1 = input("Player one, enter your move: ")
+player_2 = input("Player two, your move: ")
