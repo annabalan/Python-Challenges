740c624a263b947bfb6223ae774e0028e5967e67
Anna Balan <colnyshkasmile@gmail.com>
2018-04-16 22:18:33 -0700

    Update ex-14.py

--- Practice-Python/ex-14.py
+++ Practice-Python/ex-14.py
@@ -1,1 +1,6 @@
 #Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+def duplicates():
+    return list(set())
+
+a = [1,2,3,4,3,2,1]
+print duplicates(a)
