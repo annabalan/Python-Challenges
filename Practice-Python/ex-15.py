8a8f9822a8500830d097bb1bf0856e5e70419c1a
Anna Balan <colnyshkasmile@gmail.com>
2018-04-18 15:17:10 -0700

    Update ex-15.py

--- Practice-Python/ex-15.py
+++ Practice-Python/ex-15.py
@@ -1,1 +1,4 @@
 #Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order.
+word = input("Enter a string: ")
+def word_reverse():
+  return ' '.join(word.split()[::-1])
