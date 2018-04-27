e373eadbff972bd351a6db1966ea1f031900696b
Anna Balan <colnyshkasmile@gmail.com>
2018-04-26 21:51:05 -0700

    user instructions

--- Practice-Python/ex-18.py
+++ Practice-Python/ex-18.py
@@ -2,4 +2,8 @@
 #Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly in the correct place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” Every time the user makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.
 import random
 print("\n\n\t\t\t'Welcome to Cows & Bulls!'\n")
+print("I will generate a number, and you have to guess the numbers one digit at a time.")
+print("For every number in the wrong place, you get a cow. For every one in the right place, you get a bull.")
+print("The game will end when you get 4 bulls!")
+
 guess = int(input"Enter a 4 digit number: ")
