c0dad46bcda91f296efa26118f80c7ad72ff90ca
Anna Balan <colnyshkasmile@gmail.com>
2018-05-04 15:17:53 -0700

    option 2

--- Practice-Python/ex-16.py
+++ Practice-Python/ex-16.py
@@ -1,11 +1,12 @@
 # Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random, generating a new password every time the user asks for a new password. Include your run-time code in a main method.
 
+import secrets
+import string
+alphabet = string.ascii_letters + string.digits
+password = ''.join(secrets.choice(alphabet) for i in range(8) # for a 8character password
+#option 2
 #import random
 #password = [ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+=]
 #passlen = 8
 #pass =  "".join(random.sample(password,passlen ))
 #print pass
-import secrets
-import string
-alphabet = string.ascii_letters + string.digits
-password = ''.join(secrets.choice(alphabet) for i in range(8) # for a 8character password
