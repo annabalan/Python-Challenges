cee310e984800674c8a312f915ce4c3500e3e609
Anna Balan <colnyshkasmile@gmail.com>
2018-05-22 10:22:47 -0700

    Update ex-19.py

--- Practice-Python/ex-19.py
+++ Practice-Python/ex-19.py
@@ -7,3 +7,8 @@ from bs4 import BeautifulSoup
 base_url = 'http://www.nytimes.com'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text)
+
+all_p_cn_text_body = soup.select("div.parbase.cn_text > div.body > p")
+
+for elem in all_p_cn_text_body[7:]:
+  print(elem.text)
