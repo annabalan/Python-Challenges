0a522445f07a64574c92dddb19023430f0b8dfb1
Anna Balan <colnyshkasmile@gmail.com>
2018-05-24 13:16:49 -0700

    Update ex-20.py

--- Practice-Python/ex-20.py
+++ Practice-Python/ex-20.py
@@ -1,1 +1,6 @@
 #Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number. The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+def find(ordered_list, element_to_find):
+  for element in ordered_list:
+    if element == element_to_find:
+      return True
+  return False
