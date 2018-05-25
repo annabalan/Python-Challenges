bb7bac4b28c67d997d15121e0d2cf4afccb466b7
Anna Balan <colnyshkasmile@gmail.com>
2018-05-25 14:43:50 -0700

    Update ex-20.py

--- Practice-Python/ex-20.py
+++ Practice-Python/ex-20.py
@@ -4,3 +4,10 @@ def find(ordered_list, element_to_find):
     if element == element_to_find:
       return True
   return False
+
+if __name__=="__main__":
+  l = [2, 4, 6, 8, 10]
+  print(find(l, 5)) # prints False
+  print(find(l, 10)) # prints True
+  print(find(l, -1)) # prints False
+  print(find(l, 2)) # prints True
