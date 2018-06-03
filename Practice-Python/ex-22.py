6b25bbd359661e2e0e1feecfc9ebbff4470315b9
Anna Balan <colnyshkasmile@gmail.com>
2018-06-02 22:11:24 -0700

    Update ex-22.py

--- Practice-Python/ex-22.py
+++ Practice-Python/ex-22.py
@@ -1,3 +1,6 @@
 #Given a .txt file that has a list of a bunch of names, count how many of each name there are in the file, and print out the results to the screen. I have a .txt file for you, if you want to use it!
 
 counter_dict = {}
+
+with open('nameslist.txt') as f:
+	
